7ccec6afff605c021f08a1683f4583fe4f17bd6d
alex <90472869+alexscott06@users.noreply.github.com>
2021-10-29 14:46:50 -0400

    Update variables_practice.py
    Printed lives_remaining, forty_time, death_cry.

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,1 +1,12 @@
-#Variables Practice, Alex Scott, 10/29/21 2:22pm, version 0.0
+#Variables Practice, Alex Scott, 10/29/21 2:44, version 0.1
+
+# Three Common Types of Data 
+
+lives_remaining = 3 # INTEGERS, any +/- WHOLE number and 0.
+forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal. 
+death_cry = "I'll get you next time!" 
+# Line 7 is a STRING -- collection of letters/numbers/ other chars.
+
+print(lives_remaining)
+print(forty_time)
+print(death_cry)
